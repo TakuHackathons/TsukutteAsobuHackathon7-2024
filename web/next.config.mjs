82e778d28192e5dc92e33a8c1c9e78cd50a3a355
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-09-21 16:51:46 +0900

    setup next project and github pages.

---
+++ web/next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  // for Github Pages
+  basePath: "/TsukutteAsobuHackathon7-2024",
+  trailingSlash: true,
+};
+
+export default nextConfig;
