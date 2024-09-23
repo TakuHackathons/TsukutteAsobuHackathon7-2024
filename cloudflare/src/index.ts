b6db6116d94433005a8718045cc46a8a634831e7
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-09-24 00:37:09 +0900

    setup use groq api in cloudflare.

--- cloudflare/src/index.ts
+++ cloudflare/src/index.ts
@@ -1,9 +1,37 @@
-import { Hono } from 'hono'
+import { Context, Hono } from 'hono';
+import { env } from 'hono/adapter';
+import _ from 'lodash';
+import Groq from 'groq-sdk';
 
-const app = new Hono()
+const app = new Hono();
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text('Hello Hono!');
+});
 
-export default app
+function initGroqInstance(context: Context): Groq {
+  const { GROQ_API_KEY } = env(context);
+  return new Groq({ apiKey: GROQ_API_KEY as string });
+}
+
+app.get('/models', async (c: Context) => {
+  const groq = initGroqInstance(c);
+  const models = await groq.models.list();
+  return c.json(_.sortBy(models.data, (model) => model.created));
+});
+
+app.get('/chat', async (c) => {
+  const groq = initGroqInstance(c);
+  const chatCompletion = await groq.chat.completions.create({
+    messages: [
+      {
+        role: 'user',
+        content: 'ずんだもんについて日本語でこたえて',
+      },
+    ],
+    model: 'llama-3.1-70b-versatile',
+  });
+  return c.json(chatCompletion);
+});
+
+export default app;
