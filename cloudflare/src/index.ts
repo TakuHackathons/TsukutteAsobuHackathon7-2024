3039d36e8db0a809f197e8ed46e9a1b4a920112c
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-09-24 23:43:18 +0900

    Groq を使った対話型のレスポンスを返すAPIを作成した

--- cloudflare/src/index.ts
+++ cloudflare/src/index.ts
@@ -1,9 +1,11 @@
 import { Context, Hono } from 'hono';
+import { cors } from 'hono/cors';
 import { env } from 'hono/adapter';
 import _ from 'lodash';
 import Groq from 'groq-sdk';
 
 const app = new Hono();
+app.use('*', cors());
 
 app.get('/', (c) => {
   return c.text('Hello Hono!');
@@ -20,18 +22,29 @@ app.get('/models', async (c: Context) => {
   return c.json(_.sortBy(models.data, (model) => model.created));
 });
 
-app.get('/chat', async (c) => {
-  const groq = initGroqInstance(c);
-  const chatCompletion = await groq.chat.completions.create({
-    messages: [
-      {
-        role: 'user',
-        content: 'ずんだもんについて日本語でこたえて',
-      },
-    ],
-    model: 'llama-3.1-70b-versatile',
-  });
-  return c.json(chatCompletion);
+app.post('/chat', async (c) => {
+  const bodyText = await c.req.text();
+  if (bodyText) {
+    let bodyJson;
+    try {
+      bodyJson = JSON.parse(bodyText);
+    } catch (e) {}
+    if (!bodyJson) {
+      return c.status(200);
+    }
+    const groq = initGroqInstance(c);
+    const chatCompletion = await groq.chat.completions.create({
+      messages: [
+        {
+          role: 'user',
+          content: bodyJson.message,
+        },
+      ],
+      model: 'llama-3.1-70b-versatile',
+    });
+    return c.json(chatCompletion);
+  }
+  return c.status(200);
 });
 
 export default app;
